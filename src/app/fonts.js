b2fe83e0a2bb1cd3819bc76a46ba3e3b3b99e4d8
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-05-08 18:54:57 +0530

    5

---
+++ src/app/fonts.js
@@ -0,0 +1,17 @@
+import { Inter, JetBrains_Mono } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+  preload: true,
+  weight: ["300","400","500","600"],
+});
+
+export const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+  preload: false,
+  weight: ["400","500"],
+});
